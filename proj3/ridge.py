cbe74e895cd3899bac43e8717f1ef3fc39a5c00a
Sara Rosato <sararosato.r@gmail.com>
2024-01-14 12:26:25 +0100

    almost finished

--- proj3/ridge.py
+++ proj3/ridge.py
@@ -2,12 +2,25 @@ import numpy as np
 
 class Ridge_():
     def __init__(self, lambda_val) -> None:
+        '''
+        Initializes the parameters:
+        Weights: They are attached with every feature (input) and they convey
+        the importance of that corresponding feature in predicting
+        the final output.
+        lambda_val: Hyper-parameter that controls the strength of the regularization
+        '''
         self.lambda_val = lambda_val
         self.weights = None
 
     def ridge_fit(self, X, y):
+        '''
+        Implement the ridge regression
+        Inputs:
+        X - input features matrix
+        y - vector of labels
+        '''  
         I = np.identity(X.shape[1])
-        # I[0][0] = 0 not to penalize the y intercept
+        I[0][0] = 0 # not to penalize the y intercept
         penalty = self.lambda_val * I
         X_T = np.transpose(X)
         X_T_X = X_T @ X
@@ -19,7 +32,15 @@ class Ridge_():
         return self.weights
     
     def ridge_predict(self, X_t):
+        '''
+        To classify a new point:
+        - returns 1 if correct classification
+        - 0 otherwise
+        Input:
+        X - input matrix of dimension N x D
+        '''
         if X_t.shape[1] != len(self.weights):
             raise ValueError("Cannot perform matrix multiplication in ridge_predict")
-        predictions = X_t.dot(self.weights.to_numpy())
+        # predictions = X_t.dot(self.weights.to_numpy())
+        predictions = X_t.dot(self.weights)
         return predictions
